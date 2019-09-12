4f323bc6688b427cf5cf4682f8d996a74e99416d
Nik Molnar <nikmd23@gmail.com>
2019-09-12 19:48:11 +0000

    simple change

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const express = require('express');
 const PORT = 3000;
 const HOST = '0.0.0.0';
 
-// App
+// App change
 const app = express();
 app.get('/', (req, res) => {
 	let name = 'world';
